e5febd1a989e34d5a540a7b568a7bc12dac32485
Buganini <buganini@gmail.com>
2009-09-30 02:13:52 +0800

    implementation of some missing function in MinGW

---
+++ mingw.c
@@ -0,0 +1,24 @@
+#include <string.h>
+
+char * strsep(char **stringp, const char *delim){
+	char *r=*stringp;
+	if(!**stringp) return NULL;
+	for(;**stringp && !index(delim, **stringp);++(*stringp));
+	**stringp=0x0;
+	(*stringp)++;
+	return r;
+}
+char * index(const char *s, int c){
+	for(;*s && *s!=c;++s);
+	if(*s) return s;
+	return NULL;
+}
+
+char * strndup(const char *str, size_t len){
+	char *r;
+	size_t l=strlen(str);
+	if(len<l)l=len;
+	r=malloc(l+1);
+	memcpy(r,str,l);
+	r[l]=0;
+	return r;
