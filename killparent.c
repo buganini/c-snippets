b4ea4cff81f95b045661197aefe241645fe6726d
Buganini Q <buganini@gmail.com>
2011-05-16 06:44:28 +0800

    note sh equivalent

--- killparent.c
+++ killparent.c
@@ -2,6 +2,11 @@
 #include <signal.h>
 #include <unistd.h>
 
+/*
+ * sh evuivalent:
+ * sh -c 'kill -SIGTERM `ps -p $$ -o ppid=`'
+*/
+
 int main(int argc, char *argv[]){
 	kill(getppid(), SIGTERM);
 	return 0;
